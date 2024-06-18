13d25ef4aade2968849994075dfe7da1785751a0
Andrey Lopes Cavalcante Mendes <129072446+AndreyCavalcante@users.noreply.github.com>
2024-06-17 23:44:52 -0300

    Delete Petkingdom directory

--- Petkingdom/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    session_destroy();
-     echo "<script>
-                        alert('Sessão finalizada!');
-                        window.location.href = 'index.html';
-                      </script>"
-?>
